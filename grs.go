b4228f9c3ad4267b306586eeaff65683afb7d5db
Toomore Chiang <toomore0929@gmail.com>
2014-08-15 20:04:46 +0800

    Split main.

---
+++ grs.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	stock := StockOption{
+		no:        "2618",
+		timestamp: time.Now().Unix(),
+		date:      time.Date(2014, time.August, 11, 0, 0, 0, 0, time.Local),
+	}
+	fmt.Println(stock.GenStockUrl())
+	store_data := stock.GetData()
+	fmt.Printf("%+v\n", store_data)
+	fmt.Printf("%+v\n", store_data.QueryTime)
+	fmt.Printf("%s\n", store_data.QueryTime["sysTime"])
+}
