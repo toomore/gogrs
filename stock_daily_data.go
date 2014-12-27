35f7a19aafa004d8df091f3615a2305d0293a00d
Toomore Chiang <toomore0929@gmail.com>
2014-12-27 18:34:36 +0800

    Add simple note.

--- stock_daily_data.go
+++ stock_daily_data.go
@@ -5,15 +5,18 @@ import (
 	"time"
 )
 
+// To get TWSE daily CSV files.
 type DailyData struct {
 	stock_no string
 	date     time.Time
 }
 
+// To render url for csv.
 func (d DailyData) Url() string {
 	return fmt.Sprintf(TWSECSV, d.date.Year(), d.date.Month(), d.stock_no, RandInt())
 }
 
+// Sub one month.
 func (d *DailyData) Round() {
 	year, month, day := d.date.Date()
 	d.date = time.Date(year, month-1, day, 0, 0, 0, 0, time.UTC)
