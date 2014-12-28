ec7158b9a641e35525a2f88854b453b18b6aa174
Toomore Chiang <toomore0929@gmail.com>
2014-12-28 17:15:16 +0800

    Add simple test.

---
+++ stock_daily_data_test.go
@@ -0,0 +1,21 @@
+package gogrs
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func assertType(t *testing.T, t1 interface{}, t2 interface{}) {
+	if reflect.TypeOf(t1) != reflect.TypeOf(t2) {
+		t.Errorf("Diff type t1(%s), t2(%s)", reflect.TypeOf(t1), reflect.TypeOf(t2))
+	}
+}
+
+func TestUrl(t *testing.T) {
+	d := &DailyData{
+		No:   "2618",
+		Date: time.Now(),
+	}
+	assertType(t, d, &DailyData{})
+}
