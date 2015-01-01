4860a4dfba11a65ddae3d6f452383ff352ef8133
Toomore Chiang <toomore0929@gmail.com>
2015-01-01 13:00:05 +0800

    Move to example.

--- stock_daily_data_test.go
+++ stock_daily_data_test.go
@@ -1,6 +1,7 @@
 package gogrs
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 	"time"
@@ -18,4 +19,35 @@ func TestURL(t *testing.T) {
 		Date: time.Now(),
 	}
 	assertType(t, d, &DailyData{})
+}
+
+func ExampleDailyData() {
+	d := DailyData{No: "2618", Date: time.Now()}
+	fmt.Println(d)
+
+	r := StockRealTime{
+		No:        "2618",
+		Timestamp: time.Now().Unix(),
+		Date:      time.Date(2014, 12, 26, 0, 0, 0, 0, time.Local),
+	}
+
+	data := r.GetData()
+	fmt.Printf("%v", data)
+
+	fmt.Println("----- Test -----\n\n")
+	//fmt.Println(d.GetData())
+	stock_data, _ := d.GetData()
+	fmt.Println(stock_data)
+
+	fmt.Println(d.URL())
+
+	d.Round()
+	fmt.Println(d.URL())
+	stock_data, _ = d.GetData()
+	fmt.Println(stock_data)
+
+	d.Round()
+	fmt.Println(d.URL())
+	stock_data, _ = d.GetData()
+	fmt.Println(stock_data)
 }
