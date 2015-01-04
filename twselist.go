5241f190d0d596ee7d7b6d5503fdb99239b857c1
Toomore Chiang <toomore0929@gmail.com>
2015-01-04 23:09:16 +0800

    Add GetData.

--- twselist.go
+++ twselist.go
@@ -1,7 +1,12 @@
 package gogrs
 
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -15,5 +20,20 @@ func (l TWSEList) baseURL() string {
 }
 
 func (l TWSEList) URL(strNo string) string {
-	return fmt.Sprintf(l.baseURL, strNo)
+	return fmt.Sprintf(l.baseURL(), strNo)
+}
+
+func (l TWSEList) GetData(strNo string) ([][]string, error) {
+	data, _ := http.Get(l.URL(strNo))
+	defer data.Body.Close()
+	dataContent, _ := ioutil.ReadAll(data.Body)
+	csvArrayContent := strings.Split(string(dataContent), "\n")
+	if len(csvArrayContent) > 5 {
+		//for i := range csvArrayContent {
+		//	fmt.Println(i, csvArrayContent[i])
+		//}
+		csvReader := csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[2:len(csvArrayContent)-5], "\n")))
+		return csvReader.ReadAll()
+	}
+	return nil, errors.New("Not enough data.")
 }
