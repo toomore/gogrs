c72c29ac6be9a7e8765b35f7f500d50bed931008
Toomore Chiang <toomore0929@gmail.com>
2015-01-04 23:27:23 +0800

    Remove baseURL.

--- host_url.go
+++ host_url.go
@@ -6,6 +6,6 @@ const (
 	TWSEHOST    string = "http://www.twse.com.tw/"
 	TWSEOTCCSV  string = "/ch/stock/aftertrading/daily_trading_info/st43_download.php?d=%d/%02d&stkno=%s&r=%d"
 	TWSECSV     string = "/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/Report%d%02d/%d%02d_F3_1_8_%s.php&type=csv&r=%d"
-	TWSELISTCSV string = "/ch/trading/exchange/MI_INDEX/MI_INDEX2_print.php?genpage=genpage/Report%d%02d/A112%d%02d%02d%%s.php&type=csv"
+	TWSELISTCSV string = "/ch/trading/exchange/MI_INDEX/MI_INDEX2_print.php?genpage=genpage/Report%d%02d/A112%d%02d%02d%s.php&type=csv"
 	TWSEREAL    string = "/stock/api/getStockInfo.jsp?ex_ch=%s_%s.tw_%s&json=1&delay=0&_=%d"
 )
--- twselist.go
+++ twselist.go
@@ -15,14 +15,10 @@ type TWSEList struct {
 	Date time.Time
 }
 
-func (l TWSEList) baseURL() string {
-	year, month, day := l.Date.Date()
-	return fmt.Sprintf("%s%s", TWSEHOST, fmt.Sprintf(TWSELISTCSV, year, month, year, month, day))
-}
-
 // URL is to render urlpath.
 func (l TWSEList) URL(strNo string) string {
-	return fmt.Sprintf(l.baseURL(), strNo)
+	year, month, day := l.Date.Date()
+	return fmt.Sprintf("%s%s", TWSEHOST, fmt.Sprintf(TWSELISTCSV, year, month, year, month, day, strNo))
 }
 
 // GetData is to get csv data.
