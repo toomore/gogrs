1f9ef171f381650e0c35d96c6f039848e6cb4f9f
Toomore Chiang <toomore0929@gmail.com>
2015-01-04 20:45:51 +0800

    Add URL.

--- twselist.go
+++ twselist.go
@@ -9,7 +9,11 @@ type TWSEList struct {
 	Date time.Time
 }
 
-func (l TWSEList) URL() string {
+func (l TWSEList) baseURL() string {
 	year, month, day := l.Date.Date()
 	return fmt.Sprintf("%s%s", TWSEHOST, fmt.Sprintf(TWSELISTCSV, year, month, year, month, day))
+}
+
+func (l TWSEList) URL(strNo string) string {
+	return fmt.Sprintf(l.baseURL, strNo)
 }
