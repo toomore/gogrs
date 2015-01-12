14d4caf94f349d0cd41966cd93f091a66d708606
Toomore Chiang <toomore0929@gmail.com>
2015-01-13 03:05:37 +0800

    Add more cover test.

--- twselist_test.go
+++ twselist_test.go
@@ -2,18 +2,27 @@ package gogrs
 
 import (
 	"fmt"
+	"testing"
 	"time"
 )
 
 func ExampleTWSEList_URL() {
 	l := TWSEList{
-		//Date: time.Now(),
 		Date: time.Date(2014, 12, 26, 0, 0, 0, 0, time.Local),
 	}
 	fmt.Println(l.URL("15"))
 	// Output: http://www.twse.com.tw//ch/trading/exchange/MI_INDEX/MI_INDEX2_print.php?genpage=genpage/Report201412/A1122014122615.php&type=csv
 }
-func ExampleTWSEList_GetData() {
+func TestTWSEList_GetData_notEnoughData(*testing.T) {
+	year, month, day := time.Now().Date()
+	l := TWSEList{
+		Date: time.Date(year, month+1, day, 0, 0, 0, 0, time.Local),
+	}
+	listdata, err := l.GetData("15") //航運業
+	fmt.Println(listdata, err)
+}
+
+func TestTWSEList_GetData(*testing.T) {
 	l := TWSEList{
 		//Date: time.Now(),
 		Date: time.Date(2014, 12, 26, 0, 0, 0, 0, time.Local),
