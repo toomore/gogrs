9f429ab0d1e9dd2a7c5e1c9d5c84a1546e4c3ca0
Toomore Chiang <toomore0929@gmail.com>
2015-04-01 00:41:58 +0800

    Remove bin.

--- cmd/main.go
+++ cmd/main.go
@@ -7,13 +7,22 @@ import (
 	"github.com/toomore/gogrs"
 )
 
-var stock2618 = &gogrs.DailyData{
-	No:   "2618",
-	Date: time.Date(2015, 03, 26, 0, 0, 0, 0, time.Local),
+var stock = &gogrs.DailyData{
+	No:   "2329",
+	Date: time.Date(2015, 03, 20, 0, 0, 0, 0, time.Local),
 }
 
 func main() {
-	stock2618.GetData()
-	fmt.Println(stock2618.RawData)
-	fmt.Println(stock2618.GetVolumeList())
+	stock.GetData()
+	fmt.Println(stock.RawData)
+	fmt.Println(stock.MA(6))
+	fmt.Println(stock.MAV(6))
+	fmt.Println(stock.GetPriceList())
+	fmt.Println(gogrs.ThanPastFloat64(stock.GetPriceList(), 3, true))
+	fmt.Println(gogrs.ThanPastFloat64(stock.GetPriceList(), 3, false))
+	fmt.Println(stock.GetVolumeList())
+	fmt.Println(gogrs.ThanPastUint64(stock.GetVolumeList(), 3, true))
+	fmt.Println(gogrs.ThanPastUint64(stock.GetVolumeList(), 3, false))
+	fmt.Println(stock.GetRangeList())
+	fmt.Println(stock.IsRed())
 }
