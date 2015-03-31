b7ebd14ebfd8f95a0e64daead6f054d2f3c337ae
Toomore Chiang <toomore0929@gmail.com>
2015-03-31 12:48:24 +0800

    Add bench.

--- utils_test.go
+++ utils_test.go
@@ -21,3 +21,17 @@ func BenchmarkAvgFloat64(t *testing.B) {
 		AvgFlast64(sample)
 	}
 }
+
+func BenchmarkThanPast(t *testing.B) {
+	var sample = []float64{20.2, 20.3, 100.25, 100.75}
+	for i := 0; i < t.N; i++ {
+		thanPast(sample, true)
+	}
+}
+
+func BenchmarkThanPastFloat64(t *testing.B) {
+	var sample = []float64{20.2, 20.3, 100.25, 100.75}
+	for i := 0; i < t.N; i++ {
+		ThanPastFloat64(sample, 3, true)
+	}
+}
