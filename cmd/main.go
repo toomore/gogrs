6ca0094047834cb269367c40833ec515e4d8b5b9
Toomore Chiang <toomore0929@gmail.com>
2015-04-05 01:27:08 +0800

    Fixed cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,25 +4,33 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/toomore/gogrs"
+	"github.com/toomore/gogrs/twse"
+	"github.com/toomore/gogrs/utils"
 )
 
-var stock = &gogrs.DailyData{
+var stock = &twse.DailyData{
 	No:   "2329",
-	Date: time.Date(2015, 03, 20, 0, 0, 0, 0, time.Local),
+	Date: time.Date(2015, 3, 20, 0, 0, 0, 0, time.Local),
 }
 
-func main() {
-	stock.GetData()
+func ShowAll(stock *twse.DailyData) {
 	fmt.Println(stock.RawData)
 	fmt.Println(stock.MA(6))
 	fmt.Println(stock.MAV(6))
 	fmt.Println(stock.GetPriceList())
-	fmt.Println(gogrs.ThanPastFloat64(stock.GetPriceList(), 3, true))
-	fmt.Println(gogrs.ThanPastFloat64(stock.GetPriceList(), 3, false))
+	fmt.Println(utils.ThanPastFloat64(stock.GetPriceList(), 3, true))
+	fmt.Println(utils.ThanPastFloat64(stock.GetPriceList(), 3, false))
 	fmt.Println(stock.GetVolumeList())
-	fmt.Println(gogrs.ThanPastUint64(stock.GetVolumeList(), 3, true))
-	fmt.Println(gogrs.ThanPastUint64(stock.GetVolumeList(), 3, false))
+	fmt.Println(utils.ThanPastUint64(stock.GetVolumeList(), 3, true))
+	fmt.Println(utils.ThanPastUint64(stock.GetVolumeList(), 3, false))
 	fmt.Println(stock.GetRangeList())
 	fmt.Println(stock.IsRed())
+}
+
+func main() {
+	stock.GetData()
+	ShowAll(stock)
+	fmt.Println("-----------------------------")
+	stock.PlusData()
+	ShowAll(stock)
 }
