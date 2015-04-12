18589b6e930fb23039a6e41a8817a098f1ce4062
Toomore Chiang <toomore0929@gmail.com>
2015-04-12 16:32:00 +0800

    Fixed some example.

--- twse/twselist_test.go
+++ twse/twselist_test.go
@@ -2,32 +2,36 @@ package twse
 
 import (
 	"fmt"
-	"testing"
 	"time"
+
+	"github.com/toomore/gogrs/utils"
 )
+
+func ExampleLists_Get() {
+	l := &Lists{
+		Date: time.Date(2014, 12, 26, 0, 0, 0, 0, utils.TaipeiTimeZone),
+	}
+	listdata, _ := l.Get("15") //航運業
+	fmt.Println(listdata[0])
+	// output:
+	// [2208 台船 729,340 324 12,048,156 16.45 16.60 16.45 16.45  0.00 16.45 67 16.50 58 41.13]
+}
+
+func ExampleLists_Get_notEnoughData() {
+	year, month, day := time.Now().Date()
+	l := &Lists{
+		Date: time.Date(year, month+1, day, 0, 0, 0, 0, utils.TaipeiTimeZone),
+	}
+	_, err := l.Get("15") //航運業
+	fmt.Println(err)
+	// output:
+	// Not enough data.
+}
 
 func ExampleLists_URL() {
 	l := &Lists{
-		Date: time.Date(2014, 12, 26, 0, 0, 0, 0, time.Local),
+		Date: time.Date(2014, 12, 26, 0, 0, 0, 0, utils.TaipeiTimeZone),
 	}
 	fmt.Println(l.URL("15"))
-	// Output: http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX2_print.php?genpage=genpage/Report201412/A1122014122615.php&type=csv
-}
-
-func TestLists_Get_notEnoughData(*testing.T) {
-	year, month, day := time.Now().Date()
-	l := &Lists{
-		Date: time.Date(year, month+1, day, 0, 0, 0, 0, time.Local),
-	}
-	listdata, err := l.Get("15") //航運業
-	fmt.Println(listdata, err)
-}
-
-func TestLists_Get(*testing.T) {
-	l := &Lists{
-		//Date: time.Now(),
-		Date: time.Date(2014, 12, 26, 0, 0, 0, 0, time.Local),
-	}
-	listdata, err := l.Get("15") //航運業
-	fmt.Println(listdata, err)
+	// output: http://www.twse.com.tw/ch/trading/exchange/MI_INDEX/MI_INDEX2_print.php?genpage=genpage/Report201412/A1122014122615.php&type=csv
 }
