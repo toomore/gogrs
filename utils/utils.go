086b6a72c526ed3b316e7d1d69bb2f2716e68dba
Toomore Chiang <toomore0929@gmail.com>
2015-04-13 23:56:33 +0800

    Fixed `TWSELISTCSV` line.

--- utils/utils.go
+++ utils/utils.go
@@ -20,7 +20,7 @@ const (
 	OTCHOST     string = "http://www.tpex.org.tw"
 	OTCCSV      string = "/ch/stock/aftertrading/daily_trading_info/st43_download.php?d=%d/%02d&stkno=%s&r=%d" // year, mon, stock, rand
 	TWSECSV     string = "/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?genpage=genpage/Report%d%02d/%d%02d_F3_1_8_%s.php&type=csv&r=%d"
-	TWSELISTCSV string = "/ch/trading/exchange/MI_INDEX/MI_INDEX2_print.php?genpage=genpage/Report%d%02d/A112%d%02d%02d%s.php&type=csv"
+	TWSELISTCSV string = "/ch/trading/exchange/MI_INDEX/MI_INDEX.php" // year, mon, day, type
 	TWSEREAL    string = "/stock/api/getStockInfo.jsp?ex_ch=%s_%s.tw_%s&json=1&delay=0&_=%d"
 )
 
