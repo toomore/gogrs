0e08ac646278ff3975744b0631b34a39b3b53b31
Toomore Chiang <toomore0929@gmail.com>
2015-04-15 23:37:58 +0800

    Tiny changed in goclean.sh

--- goclean.sh
+++ goclean.sh
@@ -39,3 +39,6 @@ go tool cover -func profile.cov
 # To submit the test coverage result to coveralls.io,
 # use goveralls (https://github.com/mattn/goveralls)
 # goveralls -coverprofile=profile.cov -service=travis-ci
+
+# Test build cmd.
+cd ./cmd; go build ./; time ./cmd; rm -rf ./cmd;
