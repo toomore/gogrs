32f1871d517b96c45b606b3c6d0ea85e59c064d8
Toomore Chiang <toomore0929@gmail.com>
2015-04-18 01:19:00 +0800

    Add`readCSV` for test.

--- tradingdays/tradingdays.go
+++ tradingdays/tradingdays.go
@@ -1,10 +1,29 @@
 package tradingdays
 
-import "time"
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"time"
+)
 
 func IsOpen(d time.Time) bool {
 	if d.Weekday() == 0 || d.Weekday() == 6 {
 		return false
 	}
 	return true
+}
+
+func readCSV() {
+	data, _ := ioutil.ReadFile("./list.csv")
+	csvdata := csv.NewReader(bytes.NewReader(data))
+	for {
+		record, err := csvdata.Read()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(record[1])
+	}
 }
