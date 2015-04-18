ae47b460bff53e4a2a08eedf25b668792e2b9a79
Toomore Chiang <toomore0929@gmail.com>
2015-04-18 12:17:13 +0800

    Add `exceptDays`.

--- tradingdays/tradingdays.go
+++ tradingdays/tradingdays.go
@@ -3,12 +3,14 @@ package tradingdays
 import (
 	"bytes"
 	"encoding/csv"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"time"
+
+	"github.com/toomore/gogrs/utils"
 )
 
+// IsOpen is check open or not.
 func IsOpen(d time.Time) bool {
 	if d.Weekday() == 0 || d.Weekday() == 6 {
 		return false
@@ -16,14 +18,27 @@ func IsOpen(d time.Time) bool {
 	return true
 }
 
+var exceptDays map[int64]bool
+var timeLayout = "2006/1/02"
+
 func readCSV() {
 	data, _ := ioutil.ReadFile("./list.csv")
 	csvdata := csv.NewReader(bytes.NewReader(data))
+	exceptDays = make(map[int64]bool)
+
 	for {
 		record, err := csvdata.Read()
 		if err == io.EOF {
 			break
 		}
-		fmt.Println(record[1])
+		//fmt.Println(record[1])
+		t, err := time.ParseInLocation(timeLayout, record[0], utils.TaipeiTimeZone)
+		isopen := false
+		if record[1] == "1" {
+			isopen = true
+		}
+		exceptDays[t.Unix()] = isopen
+		//fmt.Println(t, err, t.Unix(), isopen)
+		//fmt.Println(exceptDays)
 	}
 }
