ab5c87c6bb811d8583be96ba41432d0f3dab1c1e
Toomore Chiang <toomore0929@gmail.com>
2015-04-20 23:33:08 +0800

    Add Log.

--- tradingdays/server/main.go
+++ tradingdays/server/main.go
@@ -5,12 +5,23 @@ import (
 	"net/http"
 )
 
+func Log(req *http.Request) {
+	log.Println(req.URL, req.UserAgent(), req.Form)
+}
+
 func Home(w http.ResponseWriter, req *http.Request) {
-	log.Println(req.FormValue("q"))
 	w.Write([]byte("Hello World"))
+	Log(req)
+}
+
+func TradeOpen(w http.ResponseWriter, req *http.Request) {
+	log.Println(req.FormValue("q"))
+	w.Write([]byte(req.FormValue("q")))
+	Log(req)
 }
 
 func main() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/open", TradeOpen)
 	log.Fatal(http.ListenAndServe(":59123", nil))
 }
