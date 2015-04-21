d2894829ac3b1fa665bf784124c3d1c34e600168
Toomore Chiang <toomore0929@gmail.com>
2015-04-21 20:35:59 +0800

    Add flag for http.

--- tradingdays/tradingdays_server/main.go
+++ tradingdays/tradingdays_server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
 	"strconv"
@@ -47,12 +48,16 @@ func TradeOpen(w http.ResponseWriter, req *http.Request) {
 	Log(req)
 }
 
+var httpport string
+
 func init() {
 	tradingdays.DownloadCSV(true)
+	flag.StringVar(&httpport, "http", ":59123", "HTTP service address (e.g., ':59123')")
 }
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/open", TradeOpen)
-	log.Fatal(http.ListenAndServe(":59123", nil))
+	log.Fatal(http.ListenAndServe(httpport, nil))
 }
