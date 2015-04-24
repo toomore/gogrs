8155828af701db4dfc0600c8ca199175cbd66409
Toomore Chiang <toomore0929@gmail.com>
2015-04-24 23:27:14 +0800

    Add more docs for tradingdays_server.

--- tradingdays/tradingdays_server/main.go
+++ tradingdays/tradingdays_server/main.go
@@ -1,3 +1,35 @@
+// 提供簡單的日期查詢 API Server.
+//
+/*
+主要支援動態更新 CSV 檔案讀取，解決非預定開休市狀況（如：颱風假）
+
+Install:
+	go install -v github.com/toomore/gogrs/tradingdays/tradingdays_server
+
+Usage:
+
+	tradingdays_server [flags]
+
+The flags are:
+	-http
+			HTTP service address (default ':59123')
+	-csvcachetime
+			CSV cache time.(default: 21600)
+
+URL Path:
+	/open?q={timestamp}
+
+回傳 JSON 格式
+
+	{
+		"date": "2015-04-24T15:14:52Z",
+		"open": true
+	}
+
+範例：
+	http://gogrs-trd.toomore.net/open?q=1429888492
+
+*/
 package main
 
 import (
