feadbac641f24d68d27090559b6b7a62f011a579
Toomore Chiang <toomore0929@gmail.com>
2015-04-29 18:59:39 +0800

    Add show index.

--- cmd/realtime/main.go
+++ cmd/realtime/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/toomore/gogrs/realtime"
@@ -14,14 +16,16 @@ func TaipeiNow() time.Time {
 	return time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, utils.TaipeiTimeZone)
 }
 
-func prettyprint(data realtime.Data) {
-	log.Printf("%s(%s) $%.2f(%.2f) %.0f/%.0f [%s] [%s %s]\n", data.Info.Name, data.Info.No,
+func prettyprint(data realtime.Data) string {
+	return fmt.Sprintf("%s(%s) $%.2f(%.2f) %.0f/%.0f [%s] [%s %s]",
+		data.Info.Name, data.Info.No,
 		data.Price, data.Price-data.Open, data.Volume, data.VolumeAcc,
 		data.TradeTime, data.SysInfo["sysDate"], data.SysInfo["sysTime"])
 }
 
 var twseNo = flag.String("twse", "", "TWSE NO.")
 var otcNo = flag.String("otc", "", "OTC NO.")
+var index = flag.Bool("index", false, "Show Index.")
 
 func main() {
 	flag.Parse()
@@ -29,12 +33,32 @@ func main() {
 	if *twseNo != "" {
 		r = realtime.NewTWSE(*twseNo, TaipeiNow())
 		data, _ := r.Get()
-		prettyprint(data)
+		log.Println(prettyprint(data))
 	}
 	if *otcNo != "" {
 		r = realtime.NewOTC(*otcNo, TaipeiNow())
 		data, _ := r.Get()
-		prettyprint(data)
+		log.Println(prettyprint(data))
+	}
+	if *index {
+		var wg sync.WaitGroup
+		indexList := [3]*realtime.StockRealTime{realtime.NewWeight(TaipeiNow()),
+			realtime.NewOTCI(TaipeiNow()), realtime.NewFRMSA(TaipeiNow())}
+		result := make(chan string)
+		wg.Add(3)
+		for _, r := range indexList {
+			go func(r *realtime.StockRealTime) {
+				data, _ := r.Get()
+				result <- prettyprint(data)
+			}(r)
+		}
+		go func() {
+			for v := range result {
+				wg.Done()
+				log.Println(v)
+			}
+		}()
+		wg.Wait()
 	}
 	//flag.PrintDefaults()
 }
