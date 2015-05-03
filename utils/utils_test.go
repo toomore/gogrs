71db3c55929b8918b0b70f245194e5b0666219f7
Toomore Chiang <toomore0929@gmail.com>
2015-05-03 11:38:41 +0800

    Add commend and testing.

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -193,3 +193,20 @@ func TestCountCountine(t *testing.T) {
 		t.Error("Should be `1 false`")
 	}
 }
+
+func TestCalDiff(t *testing.T) {
+	var sampleA = []float64{10.0, 11.1, 12.2, 13.3}
+	var sampleB = []float64{12.2, 11.1, 10.0}
+	var result = CalDiff(sampleA, sampleB)
+	if result[2] != float64(13.3)-float64(10.0) {
+		t.Error("Wrong cal.")
+	}
+}
+
+func BenchmarkCalDiff(b *testing.B) {
+	var sampleA = []float64{10.0, 11.1, 12.2, 13.3}
+	var sampleB = []float64{12.2, 11.1, 10.0}
+	for i := 0; i < b.N; i++ {
+		CalDiff(sampleA, sampleB)
+	}
+}
