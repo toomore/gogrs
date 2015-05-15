8db39920f422caa590780549566f776261015f3b
Toomore Chiang <toomore0929@gmail.com>
2015-05-15 17:08:30 +0800

    Add docs.

--- cmd/twsereport/main.go
+++ cmd/twsereport/main.go
@@ -1,3 +1,24 @@
+// 每日收盤後產生符合選股條件的報告.
+//
+/*
+Install:
+
+	go install github.com/toomore/gogrs/cmd/twsereport
+
+Usage:
+
+	twsereport [flags]
+
+The flags are:
+
+	-twse
+		上市股票代碼，可使用 ',' 分隔多組代碼，例：2618,2329
+	-twsecate
+		上市股票類別，可使用 ',' 分隔多組代碼，例：11,15
+	-ncpu
+		指定 CPU 數量，預設為實際 CPU 數量
+
+*/
 package main
 
 import (
