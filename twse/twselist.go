83508a140bbe86a9a818c9e31cec665e56bbdd30
Toomore Chiang <toomore0929@gmail.com>
2015-05-17 09:35:56 +0800

    Remove duplicated code.

--- twse/twselist.go
+++ twse/twselist.go
@@ -104,33 +104,28 @@ func (l *Lists) Get(category string) ([][]string, error) {
 	dataContent, _ := iconv.ConvertString(string(dataContentCp950), "cp950", "utf-8")
 	csvArrayContent := strings.Split(dataContent, "\n")
 
+	var csvReader *csv.Reader
 	switch category {
 	case "MS":
 		if len(csvArrayContent) > 6 {
-			csvReader := csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[4:51], "\n")))
-			returnData, err := csvReader.ReadAll()
-			return returnData, err
+			csvReader = csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[4:51], "\n")))
 		}
 	case "ALLBUT0999":
 		if len(csvArrayContent) > 155 {
-			csvReader := csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[154:len(csvArrayContent)-9], "\n")))
-			returnData, err := csvReader.ReadAll()
-			if err == nil {
-				l.categoryRawData[category] = returnData
-				l.formatData(category)
-			}
-			return returnData, err
+			csvReader = csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[154:len(csvArrayContent)-9], "\n")))
 		}
 	default:
 		if len(csvArrayContent) > 9 {
-			csvReader := csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[4:len(csvArrayContent)-7], "\n")))
-			returnData, err := csvReader.ReadAll()
-			if err == nil {
-				l.categoryRawData[category] = returnData
-				l.formatData(category)
-			}
-			return returnData, err
+			csvReader = csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[4:len(csvArrayContent)-7], "\n")))
 		}
+	}
+	if csvReader != nil {
+		returnData, err := csvReader.ReadAll()
+		if err == nil {
+			l.categoryRawData[category] = returnData
+			l.formatData(category)
+		}
+		return returnData, err
 	}
 	return nil, errors.New("Not enough data.")
 }
