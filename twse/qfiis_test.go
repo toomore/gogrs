593ad6fd603b885c09f93a57dfd8f83c2c3d1052
Toomore Chiang <toomore0929@gmail.com>
2015-05-26 20:12:24 +0800

    Fixed in qfiis_test.

--- twse/qfiis_test.go
+++ twse/qfiis_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/toomore/gogrs/utils"
 )
 
-func TestQFIIS_Get(t *testing.T) {
-	qf := &QFIIS{Date: time.Date(2015, 5, 25, 0, 0, 0, 0, utils.TaipeiTimeZone)}
+func TestQFIISTOP20_Get(t *testing.T) {
+	qf := &QFIISTOP20{Date: time.Date(2015, 5, 25, 0, 0, 0, 0, utils.TaipeiTimeZone)}
 	t.Log(qf.URL())
 	t.Log(qf.Get())
 }
