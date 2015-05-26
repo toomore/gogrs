69efc19ee5207b3f0fea4a99c712b6e9dc27f394
Toomore Chiang <toomore0929@gmail.com>
2015-05-26 17:43:40 +0800

    Add QFIIS.Get.

--- twse/qfiis.go
+++ twse/qfiis.go
@@ -1,7 +1,9 @@
 package twse
 
 import (
+	"encoding/csv"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/toomore/gogrs/utils"
@@ -13,4 +15,27 @@ type QFIIS struct {
 
 func (q QFIIS) URL() string {
 	return fmt.Sprintf("%s%s", utils.TWSEHOST, fmt.Sprintf(utils.QFIISTOP20, q.Date.Year(), q.Date.Month(), q.Date.Day()))
+}
+
+func (q *QFIIS) Get() ([][]string, error) {
+	data, _ := hCache.Get(q.URL(), false)
+
+	csvArrayContent := strings.Split(string(data), "\n")
+	for i, v := range csvArrayContent[2 : len(csvArrayContent)-1] {
+		csvArrayContent[i] = strings.Replace(v, "=", "", -1)
+	}
+
+	csvReader := csv.NewReader(strings.NewReader(strings.Join(csvArrayContent[:len(csvArrayContent)-3], "\n")))
+	var (
+		allData [][]string
+		err     error
+	)
+	if allData, err = csvReader.ReadAll(); err == nil {
+		for _, v := range allData {
+			for i, vv := range v {
+				v[i] = strings.Replace(vv, ",", "", -1)
+			}
+		}
+	}
+	return allData, err
 }
--- twse/qfiis_test.go
+++ twse/qfiis_test.go
@@ -7,7 +7,8 @@ import (
 	"github.com/toomore/gogrs/utils"
 )
 
-func TestingQFIIS_Get(t *testing.T) {
+func TestQFIIS_Get(t *testing.T) {
 	qf := &QFIIS{Date: time.Date(2015, 5, 25, 0, 0, 0, 0, utils.TaipeiTimeZone)}
 	t.Log(qf.URL())
+	t.Log(qf.Get())
 }
