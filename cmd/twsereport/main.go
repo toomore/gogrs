b6a89ee60bf3924208e11879fbbc02f8a6c2f508
Toomore Chiang <toomore0929@gmail.com>
2015-06-03 21:02:05 +0800

    Add color.

--- cmd/twsereport/main.go
+++ cmd/twsereport/main.go
@@ -29,6 +29,7 @@ import (
 	"sync"
 	"time"
 
+	"github.com/fatih/color"
 	"github.com/toomore/gogrs/tradingdays"
 	"github.com/toomore/gogrs/twse"
 )
@@ -43,24 +44,48 @@ func (c *checkGroupList) Add(f checkGroup) {
 	}
 }
 
-var wg sync.WaitGroup
-var twseNo = flag.String("twse", "", "上市股票代碼，可使用 ',' 分隔多組代碼，例：2618,2329")
-var twseCate = flag.String("twsecate", "", "上市股票類別，可使用 ',' 分隔多組代碼，例：11,15")
-var ncpu = flag.Int("ncpu", runtime.NumCPU(), "指定 CPU 數量，預設為實際 CPU 數量")
-var ckList = make(checkGroupList, 1)
+var (
+	wg         sync.WaitGroup
+	twseNo     = flag.String("twse", "", "上市股票代碼，可使用 ',' 分隔多組代碼，例：2618,2329")
+	twseCate   = flag.String("twsecate", "", "上市股票類別，可使用 ',' 分隔多組代碼，例：11,15")
+	ncpu       = flag.Int("ncpu", runtime.NumCPU(), "指定 CPU 數量，預設為實際 CPU 數量")
+	ckList     = make(checkGroupList, 1)
+	white      = color.New(color.FgWhite, color.Bold).SprintfFunc()
+	red        = color.New(color.FgRed, color.Bold).SprintfFunc()
+	green      = color.New(color.FgGreen, color.Bold).SprintfFunc()
+	yellow     = color.New(color.FgYellow).SprintfFunc()
+	yellowBold = color.New(color.FgYellow, color.Bold).SprintfFunc()
+	blue       = color.New(color.FgBlue).SprintfFunc()
+)
 
 func init() {
 	runtime.GOMAXPROCS(*ncpu)
 }
 
 func prettyprint(stock *twse.Data, check checkGroup) string {
-	return fmt.Sprintf("[%s] %s %s %s $%.2f(%.2f) %d",
-		check,
-		stock.RawData[stock.Len()-1][0],
-		stock.No, stock.Name,
-		stock.GetPriceList()[len(stock.GetPriceList())-1],
-		stock.GetRangeList()[len(stock.GetRangeList())-1],
-		stock.GetVolumeList()[len(stock.GetVolumeList())-1]/1000,
+	var (
+		Price       = stock.GetPriceList()[len(stock.GetPriceList())-1]
+		RangeValue  = stock.GetRangeList()[len(stock.GetRangeList())-1]
+		Volume      = stock.GetVolumeList()[len(stock.GetVolumeList())-1] / 1000
+		outputcolor func(string, ...interface{}) string
+	)
+
+	switch {
+	case RangeValue > 0:
+		outputcolor = red
+	case RangeValue < 0:
+		outputcolor = green
+	default:
+		outputcolor = white
+	}
+
+	return fmt.Sprintf("%s %s %s %s%s %s",
+		yellow("[%s]", check),
+		blue("%s", stock.RawData[stock.Len()-1][0]),
+		outputcolor("%s %s", stock.No, stock.Name),
+		outputcolor("$%.2f", Price),
+		outputcolor("(%.2f)", RangeValue),
+		outputcolor("%d", Volume),
 	)
 }
 
@@ -93,7 +118,7 @@ func main() {
 
 	if len(datalist) > 0 {
 		for _, check := range ckList {
-			fmt.Printf("----- %v -----\n", check)
+			fmt.Println(yellowBold("----- %v -----", check))
 			wg.Add(len(datalist))
 			for _, stock := range datalist {
 				go func(check checkGroup, stock *twse.Data) {
