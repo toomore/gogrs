7510094f4013eb1e1683897a731153cba9f0a13a
Toomore Chiang <toomore0929@gmail.com>
2015-08-18 17:12:04 +0800

    Add more test.

--- cmd/twsereport/filter/filter_test.go
+++ cmd/twsereport/filter/filter_test.go
@@ -1,6 +1,20 @@
 package filter
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/toomore/gogrs/tradingdays"
+	"github.com/toomore/gogrs/twse"
+)
+
+var stock = twse.NewTWSE("2618", tradingdays.FindRecentlyOpened(time.Now()))
+
+func TestCheckGroup(t *testing.T) {
+	for i, v := range AllList {
+		t.Log(i, v.No(), v.String(), v.Mindata(), v.CheckFunc(stock))
+	}
+}
 
 func TestCheckGroup_String(t *testing.T) {
 	for i, v := range AllList {
