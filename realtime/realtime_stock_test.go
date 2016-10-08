46d746f1f2d284974c1e9782c0212110983075a4
Toomore Chiang <toomore0929@gmail.com>
2016-10-08 04:37:59 +0000

    Fixed ref fail.

--- realtime/realtime_stock_test.go
+++ realtime/realtime_stock_test.go
@@ -76,7 +76,7 @@ func ExampleStockRealTime_Get_otc() {
 	// {Exchange:otc FullName:華研國際音樂股份有限公司 Name:華研 No:8446 Ticker:8446.tw Category:32}
 }
 
-func ExampleStockRealTime_NewWeight() {
+func ExampleNewWeight() {
 	weight := NewWeight(tradingdays.FindRecentlyOpened(time.Now()))
 	data, _ := weight.Get()
 	fmt.Printf("%+v", data.Info)
@@ -84,7 +84,7 @@ func ExampleStockRealTime_NewWeight() {
 	// {Exchange:tse FullName: Name:發行量加權股價指數 No:t00 Ticker:t00.tw Category:tidx.tw}
 }
 
-func ExampleStockRealTime_NewOTCI() {
+func ExampleNewOTCI() {
 	otc := NewOTCI(tradingdays.FindRecentlyOpened(time.Now()))
 	data, _ := otc.Get()
 	fmt.Printf("%+v", data.Info)
@@ -92,7 +92,7 @@ func ExampleStockRealTime_NewOTCI() {
 	// {Exchange:otc FullName: Name:櫃買指數 No:o00 Ticker:o00.tw Category:oidx.tw}
 }
 
-func ExampleStockRealTime_NewFRMSA() {
+func ExampleNewFRMSA() {
 	farmsa := NewFRMSA(tradingdays.FindRecentlyOpened(time.Now()))
 	data, _ := farmsa.Get()
 	fmt.Printf("%+v", data.Info)
@@ -100,7 +100,7 @@ func ExampleStockRealTime_NewFRMSA() {
 	// {Exchange:tse FullName: Name:寶島股價指數 No:FRMSA Ticker:FRMSA.tw Category:tidx.tw}
 }
 
-func ExampleStockRealTime_NewTWSE() {
+func ExampleNewTWSE() {
 	twse := NewTWSE("2618", tradingdays.FindRecentlyOpened(time.Now()))
 	data, _ := twse.Get()
 	fmt.Printf("%+v", data.Info)
@@ -108,7 +108,7 @@ func ExampleStockRealTime_NewTWSE() {
 	// {Exchange:tse FullName:長榮航空股份有限公司 Name:長榮航 No:2618 Ticker:2618.tw Category:15}
 }
 
-func ExampleStockRealTime_NewOTC() {
+func ExampleNewOTC() {
 	otc := NewOTC("8446", tradingdays.FindRecentlyOpened(time.Now()))
 	data, _ := otc.Get()
 	fmt.Printf("%+v", data.Info)
