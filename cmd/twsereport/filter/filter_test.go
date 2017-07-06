b597ac35aca571f558e98b9ed9a5384dc0d53956
Toomore Chiang <toomore0929@gmail.com>
2017-07-06 08:43:22 +0000

    Fixed coverage

--- cmd/twsereport/filter/filter_test.go
+++ cmd/twsereport/filter/filter_test.go
@@ -9,10 +9,18 @@ import (
 )
 
 var stocklist = []*twse.Data{
-	twse.NewTWSE("2618", time.Date(2017, 4, 7, 0, 0, 0, 0, utils.TaipeiTimeZone)),
-	twse.NewTWSE("2618", time.Date(2017, 3, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("1201", time.Date(2016, 1, 8, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("1201", time.Date(2016, 2, 14, 0, 0, 0, 0, utils.TaipeiTimeZone)),
 	twse.NewTWSE("1201", time.Date(2017, 3, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
-	twse.NewTWSE("4938", time.Date(2017, 3, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("1201", time.Date(2017, 4, 12, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("2412", time.Date(2015, 7, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("2412", time.Date(2015, 9, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("2618", time.Date(2015, 3, 8, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("2618", time.Date(2016, 1, 8, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("2618", time.Date(2016, 2, 14, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("2618", time.Date(2017, 3, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("2618", time.Date(2017, 4, 7, 0, 0, 0, 0, utils.TaipeiTimeZone)),
+	twse.NewTWSE("4938", time.Date(2016, 3, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
 	//twse.NewTWSE("4938", time.Date(2017, time.Now().Month()+1, 21, 0, 0, 0, 0, utils.TaipeiTimeZone)),
 }
 
