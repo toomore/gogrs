9c8bf9354e2f3ce3fd940f5e9552c96c628a2e56
Toomore Chiang <toomore0929@gmail.com>
2017-07-07 07:01:25 +0000

    Add gogrs example

---
+++ gogrs/cmd/example.go
@@ -0,0 +1,69 @@
+// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/toomore/gogrs/tradingdays"
+	"github.com/toomore/gogrs/twse"
+	"github.com/toomore/gogrs/utils"
+)
+
+// exampleCmd represents the example command
+var exampleCmd = &cobra.Command{
+	Use:   "example",
+	Short: "Show example",
+	Long:  `Show gogrs example.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var stock = twse.NewTWSE("2618", time.Date(2015, 3, 20, 0, 0, 0, 0, time.Local))
+		stock.Get()
+		showAll(stock)
+		fmt.Println("-----------------------------")
+		stock.PlusData()
+		showAll(stock)
+		fmt.Println("-----------------------------")
+		fmt.Println(tradingdays.IsOpen(2015, 5, 1))
+	},
+}
+
+func showAll(stock *twse.Data) {
+	fmt.Println(stock.RawData)
+	fmt.Println(stock.MA(6))
+	fmt.Println(stock.MAV(6))
+	fmt.Println(stock.GetPriceList())
+	fmt.Println(utils.ThanPastFloat64(stock.GetPriceList(), 3, true))
+	fmt.Println(utils.ThanPastFloat64(stock.GetPriceList(), 3, false))
+	fmt.Println(stock.GetVolumeList())
+	fmt.Println(utils.ThanPastUint64(stock.GetVolumeList(), 3, true))
+	fmt.Println(utils.ThanPastUint64(stock.GetVolumeList(), 3, false))
+	fmt.Println(stock.GetRangeList())
+	fmt.Println(stock.IsRed())
+}
+
+func init() {
+	RootCmd.AddCommand(exampleCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// exampleCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// exampleCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
