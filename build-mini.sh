34cd15d0c7fa4387bcefe48b1a63bc69c2c82ac0
Toomore Chiang <toomore0929@gmail.com>
2019-04-14 15:51:59 +0800

    Update travis

--- build-mini.sh
+++ build-mini.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -it --rm -v $(pwd)/gogrs_bin:/gogrs_bin toomore/gogrs:latest \
-    sh -c "go get -v ./...;
+    sh -c "GO111MODULE=on go get -v ./...;
            cp /go/bin/gogrs /gogrs_bin;"
 
 docker build -t toomore/gogrs:mini -f ./Dockerfile-mini ./
